6db2f3e6ed29fc029c7e7dbeb44babc05d6ef636
Chris <chrisaukema@gmail.com>
2015-11-04 16:28:37 +0100

    JSHint

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
-var jslint = require('gulp-jslint');
+var jshint = require('gulp-jshint');
 
 var paths = {
   sass: ['./scss/**/*.scss']
@@ -36,6 +36,12 @@ gulp.task('install', ['git-check'], function() {
     .on('log', function(data) {
       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
     });
+});
+
+gulp.task('lint', function() {
+  return gulp.src('./lib/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('YOUR_REPORTER_HERE'));
 });
 
 gulp.task('git-check', function(done) {
