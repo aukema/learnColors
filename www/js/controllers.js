a44aa176bb8d93d565590cc66d58a96b985fcd16
Chris <chrisaukema@gmail.com>
2015-11-04 15:37:22 +0100

    travis test

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($log, $scope) {
   $scope.colors = [
-  {name: 'Rood', css: 'assertive'}, 
+  {name: 'Rood', css: 'assertive' }, 
   {name: 'Geel', css: 'energized'}, 
   {name: 'Groen', css: 'balanced'},
   {name: 'Blauw', css: 'positive'},
